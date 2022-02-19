84ca0ebc10ed866c4b41f0967e5c9685bf7d1456
YuwenLu <luyuwen2000@163.com>
2022-02-19 23:59:15 +0800

    初始化项目

---
+++ src/main/java/com/reecelu/pmsserver/PmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.reecelu.pmsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsServerApplication.class, args);
+    }
+
+}
