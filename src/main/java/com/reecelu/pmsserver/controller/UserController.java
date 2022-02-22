afd09a12d280f269cd68555adc9e997f6abeee8e
YuwenLu <luyuwen2000@163.com>
2022-02-22 22:29:40 +0800

    实现了增改数据库，使用postman测试

--- src/main/java/com/reecelu/pmsserver/controller/UserController.java
+++ src/main/java/com/reecelu/pmsserver/controller/UserController.java
@@ -2,22 +2,31 @@ package com.reecelu.pmsserver.controller;
 
 import com.reecelu.pmsserver.entity.User;
 import com.reecelu.pmsserver.mapper.UserMapper;
+import com.reecelu.pmsserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private UserService userService;
 
-    @GetMapping("/")
+    @PostMapping
+    public Integer save(@RequestBody User user){
+        return userMapper.insert(user);
+    }
+
+
+    @GetMapping()
     public List<User> index(){
-
+        List<User> all= userMapper.findAll();
         return userMapper.findAll();
     }
 }
--- src/main/java/com/reecelu/pmsserver/mapper/UserMapper.java
+++ src/main/java/com/reecelu/pmsserver/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package com.reecelu.pmsserver.mapper;
 
 import com.reecelu.pmsserver.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -13,4 +15,11 @@ public interface UserMapper {
     @Select("select * from sys_user")
     List<User> findAll();
 
+    @Insert("INSERT into sys_user(username,password,nickname,email,phone,address)VALUES" +
+            "(#{username},#{password},#{nickname},#{email},#{phone},#{address})")
+    int insert(User user);
+
+    @Update("update sys_user set username=#{username},password=#{password},nickname=#{nickname},email=#{email}" +
+            ",phone=#{phone},address=#{address} where id =#{id}")
+    int update(User user);
 }
