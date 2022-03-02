6dd71b7300db405dded00d8de60b94cec8c47b60
YuwenLu <luyuwen2000@163.com>
2022-03-02 22:12:57 +0800

    实现了报修维修页中维修信息的修改功能(接口/pms/reportrepairsearch/editstatus)；添补了相关注释

--- src/main/java/com/reecelu/pmsserver/dao/ReportRepairSearchDao.java
+++ src/main/java/com/reecelu/pmsserver/dao/ReportRepairSearchDao.java
@@ -10,9 +10,12 @@ import java.util.List;
 @Mapper
 public interface ReportRepairSearchDao {
 
-    //指向mapper中的reportRepairSearch.xml，使用reportRepairSearch中 id="reportRepairSearch" 的SQL语句
+    //返回模糊查找结果，指向mapper中的reportRepairSearch.xml，使用reportRepairSearch中 id="reportRepairSearch" 的SQL语句
     List<ReportRepairs> reportRepairSearch(String reporter, String repairType,int pageNum, int pageSize);
 
-    //指向mapper中的reportRepairSearch.xml，使用reportRepairSearch中 id="selectTotal" 的SQL语句
+    //返回模糊查找的数据量，指向mapper中的reportRepairSearch.xml，使用reportRepairSearch中 id="selectTotal" 的SQL语句
     Integer selectTotal(String reporter,@Param("repairType") String repair_type);
+
+    //修改报修维修表中的维修状态相关属性，指向mapper中的reportRepairSearch.xml，使用reportRepairSearch中 id="editStatus" 的SQL语句
+    Integer editStatus(int id,String serviceman, @Param("repairStatus") String repair_status);
 }
