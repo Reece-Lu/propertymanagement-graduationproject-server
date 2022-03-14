5ff8d7fe23e97b31d31124a5e23a283bac3b1771
YuwenLu <luyuwen2000@163.com>
2022-03-14 21:33:43 +0800

    增加了物业查询业主档案接口(pms/proprietorinfo/propertysearchproprietorinfo)

--- src/main/java/com/reecelu/pmsserver/service/ProprietorInfoService.java
+++ src/main/java/com/reecelu/pmsserver/service/ProprietorInfoService.java
@@ -1,7 +1,10 @@
 package com.reecelu.pmsserver.service;
 
+import com.reecelu.pmsserver.controller.DTO.PropertyGetProprietorInfo;
 import com.reecelu.pmsserver.entity.Proprietor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -9,5 +12,7 @@ public interface ProprietorInfoService {
     Proprietor getProprietorInfo(int id);
 
     Integer changeProprietorInfo(int id, int attribute ,String value);
+
+    List<Proprietor> getAllProprietor(PropertyGetProprietorInfo propertyGetProprietorInfo);
 
 }
